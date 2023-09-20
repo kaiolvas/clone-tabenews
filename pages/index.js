9a40ee1556e2dbfc30394c04d1e5d76f61563cab
kaiolvas <68060437+kaiolvas@users.noreply.github.com>
2023-09-20 21:55:24 +0000

    mais uma correção

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function home() {
-    return <h1> Você e o Álvaro são minha razão para continuar </h1>
+    return <h1> Você e o Álvaro são minha razão para continuar. s2 </h1>
 }
 export default home;
